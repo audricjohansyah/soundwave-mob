247124a07a15af815e120851280df2b22d1fc71b
Alexander Audric Johansyah <audricjohansyah@gmail.com>
2023-11-27 15:21:23 +0700

    fixing back button

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -39,6 +39,7 @@ class _LoginPageState extends State<LoginPage> {
     final request = context.watch<CookieRequest>();
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: const Text('Login'),
         backgroundColor: Colors.indigo,
         foregroundColor: Colors.white,
